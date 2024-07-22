7e0ceb41f5041c06c1812aa07a85bd8fb8a7619d
pedrvisk <pedrobrbrpedro@gmail.com>
2024-07-22 09:57:10 -0300

    Tela de carregamento optimizada

--- src/components/LoadingScreen.vue
+++ src/components/LoadingScreen.vue
@@ -4,11 +4,11 @@ import { ref, onMounted } from 'vue';
 export default {
   name: 'LoadingScreen',
   setup() {
-    const isScreenLoading = ref<boolean>(false);
+    const isScreenLoading = ref<boolean>(true);
 
     onMounted(() => {
       setTimeout(() => {
-        isScreenLoading.value = true;
+        isScreenLoading.value = false;
       }, 3000);
     });
 
@@ -20,13 +20,10 @@ export default {
 </script>
 
 <template>
-  <div :class="{ 'animate-loadingScreen': isScreenLoading }">
-    <div
-      class="flex flex-col right-0 left-0 bottom-0 top-0 z-[1000] opacity-90 bg-gradient-to-bl from-white to-[#F7F4F3] dark:from-black dark:to-[#191919] fixed items-center justify-center"
-    >
-      <span
-        class="loading loading-spinner loading-lg text-black dark:text-gray-100 drop-shadow-lg"
-      />
-    </div>
+  <div
+    :class="{ '!opacity-0 invisible': !isScreenLoading }"
+    class="flex transition-all !duration-500 flex-col right-0 left-0 bottom-0 top-0 z-[1000] opacity-90 bg-gradient-to-bl from-white to-[#F7F4F3] dark:from-black dark:to-[#191919] fixed items-center justify-center"
+  >
+    <span class="loading loading-spinner loading-lg text-black dark:text-gray-100 drop-shadow-lg" />
   </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,6 @@
 export default {
   darkMode: 'class',
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  theme: {
-    extend: {
-      keyframes: {
-        loadingScreen: {
-          to: {
-            opacity: '0',
-            visibility: 'hidden'
-          }
-        }
-      },
-      animation: {
-        loadingScreen: 'loadingScreen 0.5s forwards'
-      }
-    }
-  },
+  theme: {},
   plugins: [require('tailwindcss-primeui')]
 };
